e1966686e6019bc353284debd44dc8af6184aea4
GaryChen886 <76957777+GaryChen886@users.noreply.github.com>
2023-05-28 17:20:43 +0800

    Update mac.v

--- src/mac.v
+++ src/mac.v
@@ -7,7 +7,27 @@ module mac (
     output reg [7:0] left_out,
     output reg [7:0] mat_out
 );
-
+  // Reg for bit shift offset, which ranges 0~8
+  reg[2:0]ShiftCount;
+  always @(*) begin
+    if(reset) begin
+      up_in = 8'b0;
+    end
+    else begin
+      case(up_in)
+        // shift 0 bits 
+        (up_in >= 0 && up_in )
+        // shift 1 bits
+        // shift 2 bits
+        // shift 3 bits
+        // shift 4 bits
+        // shift 5 bits
+        // shift 6 bits
+        // shift 7 bits
+        // shift 8 bits
+      endcase
+    end
+  end
   always @(posedge clk or posedge reset) begin
     //  always @ (posedge clk) begin
     if (reset) begin
