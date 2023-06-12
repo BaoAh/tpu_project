e8df9ba6e45c623083a1efb1ebf51a82d31ba10b
GaryChen886 <76957777+GaryChen886@users.noreply.github.com>
2023-06-12 21:58:14 +0800

    Update mac_PowerOfTwo_q3.v

--- src/mac_PowerOfTwo_q3.v
+++ src/mac_PowerOfTwo_q3.v
@@ -28,22 +28,6 @@ module mac (
         4 : mat_out <= mat_out +  {left_in[3:0], 4'b0};
         5 : mat_out <= mat_out +  {left_in[2:0], 5'b0};
         6 : mat_out <= mat_out +  {left_in[1:0], 6'b0};
-        //7 : mat_out <= mat_out +  {left_in[0], 7'b0};
-        // //0 : {mat_out[7:1], {left_in + mat_out[0]}};
-        // //1 : mat_out <= mat_out +  left_in << 1;
-        // 1 : {mat_out[7:2], {left_in + mat_out[1]}, mat_out[0]};
-        // //2 : mat_out <= mat_out +  left_in << 2;
-        // 2 : {mat_out[7:3], {left_in + mat_out[2]}, mat_out[1:0]};
-        // //3 : mat_out <= mat_out +  left_in << 3;
-        // 3 : {mat_out[7:4], {left_in + mat_out[3]}, mat_out[2:0]};
-        // //4 : mat_out <= mat_out +  left_in << 4;
-        // 4 : {mat_out[7:5], {left_in + mat_out[4]}, mat_out[3:0]};
-        // //5 : mat_out <= mat_out +  left_in << 5;
-        // 5 : {mat_out[7:6], {left_in + mat_out[5]}, mat_out[4:0]};
-        // //6 : mat_out <= mat_out +  left_in << 6;
-        // 6 : {mat_out[7], {left_in + mat_out[6]}, mat_out[5:0]};
-        // //7 : mat_out <= mat_out +  left_in << 7;
-        // 7 : {{left_in + mat_out[7]}, mat_out[6:0]};
         default: mat_out <= mat_out +  left_in;
       endcase
       //mat_out  <= mat_out + up_in * left_in;
